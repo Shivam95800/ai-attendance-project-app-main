364e9305eb8dc13bd02ddd2b644cbb9c60f508e2
Shivam Soni <shivam301102@gmail.com>
2026-07-30 14:37:23 +0530

    Update config.py

--- src/database/config.py
+++ src/database/config.py
@@ -1,9 +1,10 @@
-import streamlit as st
-
-
+import os
 from supabase import create_client, Client
 
-supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("SUPABASE_URL or SUPABASE_KEY environment variables not found!")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
